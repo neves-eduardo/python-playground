9c5d14486e538b5405d6e56da79efff3c304b538
Eduardo <eduardoneves858@gmail.com>
2020-03-12 22:43:53 -0300

    feat: adding datetime to nano in NanoUtils

--- NanoUtils/NanoUtils.py
+++ NanoUtils/NanoUtils.py
@@ -1,6 +1,9 @@
 import datetime
 import time
 
+def convert_datetime_to_nano(dt):
+    return int(datetime.datetime.timestamp(dt)*1e+9)
+
 def convert_hours_to_nano(hrs):
     return hrs*3.6e12
 
@@ -9,6 +12,3 @@ def convert_nano_to_timestamp(nano):
 
 def convert_nano_to_datetime(nano):
     return datetime.datetime.fromtimestamp(convert_nano_to_timestamp(nano))
-
-print(convert_nano_to_timestamp(1584060974909259269))
-print(convert_nano_to_datetime(15839717797210000))
