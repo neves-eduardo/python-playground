5368e65618ab6210a25c6896958a0cbf40b7b8e4
Eduardo <eduardoneves858@gmail.com>
2020-03-12 23:53:28 -0300

    feat: adding timestamp to nano

--- NanoUtils/NanoUtils.py
+++ NanoUtils/NanoUtils.py
@@ -12,3 +12,8 @@ def convert_nano_to_timestamp(nano):
 
 def convert_nano_to_datetime(nano):
     return datetime.datetime.fromtimestamp(convert_nano_to_timestamp(nano))
+
+def convert_timestamp_to_nano(tm):
+    return int(tm*1e+9)
+
+print(convert_timestamp_to_nano(datetime.datetime.now().timestamp()))
